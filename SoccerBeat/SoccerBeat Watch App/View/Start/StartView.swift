f2774e47d25fc0476c4465514b531f930b7b3603
Damagucci-Juice <jsr9874@gmail.com>
2023-10-22 13:53:47 +0900

    feat: [#11] watch foldering

---
+++ SoccerBeat/SoccerBeat Watch App/View/Start/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  SoccerBeat Watch App
+//
+//  Created by Gucci on 10/22/23.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
