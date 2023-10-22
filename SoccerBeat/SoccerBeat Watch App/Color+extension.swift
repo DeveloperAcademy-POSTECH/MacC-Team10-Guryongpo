6393ea9c7870f222a48d29038a6de6d9c2fca429
Damagucci-Juice <jsr9874@gmail.com>
2023-10-22 15:36:40 +0900

    feat: [#14] Color Extension 적용

--- SoccerBeat/SoccerBeat Watch App/Color+extension.swift
+++ SoccerBeat/SoccerBeat Watch App/Color+extension.swift
@@ -5,4 +5,16 @@
 //  Created by Gucci on 10/22/23.
 //
 
-import Foundation
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    // MARK: - Color 생성자
+    
+    /// 사용법: .foregroundStyle(.init(hex: 0xFFFFFF))
+    init(hex: UInt, alpha: Double = 1.0) {
+            let red = Double((hex >> 16) & 0xff) / 255.0
+            let green = Double((hex >> 8) & 0xff) / 255.0
+            let blue = Double(hex & 0xff) / 255.0
+            self.init(red: red, green: green, blue: blue, opacity: alpha)
+    }
+}
