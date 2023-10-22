56a39deb66cb5afd14779fe444b9f7e527dacfaa
Damagucci-Juice <jsr9874@gmail.com>
2023-10-22 15:32:39 +0900

    add: Color Extension & ProgressView 생성

---
+++ SoccerBeat/SoccerBeat Watch App/Color+extension.swift
@@ -0,0 +1,8 @@
+//
+//  Color+extension.swift
+//  SoccerBeat Watch App
+//
+//  Created by Gucci on 10/22/23.
+//
+
+import Foundation
---
+++ SoccerBeat/SoccerBeat Watch App/ProgressView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressView.swift
+//  SoccerBeat Watch App
+//
+//  Created by Gucci on 10/22/23.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressView()
+}
