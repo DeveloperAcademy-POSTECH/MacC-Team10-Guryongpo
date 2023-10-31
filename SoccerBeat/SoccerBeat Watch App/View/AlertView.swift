6d0662223ca739e7cf7db5ee7ee5f032857a8dd4
Jose <dbswkgus99@snu.ac.kr>
2023-10-31 21:37:55 +0900

    feat: [#57] Watch PrecountView View Change

--- SoccerBeat/SoccerBeat Watch App/View/AlertView.swift
+++ SoccerBeat/SoccerBeat Watch App/View/AlertView.swift
@@ -9,26 +9,30 @@ import SwiftUI
 
 struct AlertView: View {
     @State private var beatAnimation: Bool = true
-    var text: String = "Zone 5!\nTake a\nBreath "
+    var text: String = "Zone 5!\nTake a\nBreath"
     var body: some View {
         ZStack {
             Image("alertButton")
                 .resizable()
                 .scaledToFill()
+                .padding()
+                .scaleEffect(beatAnimation ? 1.1 : 1)
+                .animation(.smooth.repeatForever(autoreverses: true).speed(0.7), value: beatAnimation)
+                .onAppear(perform: {
+                    withAnimation {
+                        beatAnimation.toggle()
+                    }
+                }).padding()
+            
             Text(text)
-            .font(.system(size: 30))
-            .bold()
-            .italic()
-            .multilineTextAlignment(.center)
+                .font(.system(size: 30, weight: .black))
+                .italic()
+                .kerning(-1)
+                .lineSpacing(-1.0)
+                .multilineTextAlignment(.center)
             .foregroundStyle(LinearGradient(colors: [.white, .yellow], startPoint: .leading, endPoint: .trailing))
         }
-        .scaleEffect(beatAnimation ? 1.1 : 1)
-        .animation(.smooth.repeatForever(autoreverses: true).speed(0.7), value: beatAnimation)
-        .onAppear(perform: {
-            withAnimation {
-                beatAnimation.toggle()
-            }
-        }).padding()
+
     }
 }
 
