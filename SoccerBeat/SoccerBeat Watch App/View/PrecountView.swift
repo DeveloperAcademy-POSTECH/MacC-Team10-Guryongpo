fc580bb9b8b1f63b400a115251603adcb51fc657
Jose <dbswkgus99@snu.ac.kr>
2023-10-31 20:38:43 +0900

    feat: [#57] Watch PrecountView

---
+++ SoccerBeat/SoccerBeat Watch App/View/PrecountView.swift
@@ -0,0 +1,61 @@
+//
+//  PrecountView.swift
+//  SoccerBeat Watch App
+//
+//  Created by jose Yun on 10/31/23.
+//
+
+import SwiftUI
+
+struct PrecountView: View {
+    @State var count: Int = 1
+    var body: some View {
+        VStack {
+            Text("\(count)")
+                .foregroundStyle(.precountGradient)
+                .font(
+                    Font.custom("SF Pro Text", size: 68)
+                .weight(.black))
+                .italic()
+                .overlay {
+                    
+                }
+            
+            HStack {
+                Circle()
+                    .frame(width: 11, height: 11)
+                    .foregroundStyle(.precountGradient)
+                    .opacity(0.6)
+                
+            
+                Circle()
+                    .frame(width: 11, height: 11)
+                    .foregroundStyle(.precountGradient)
+                    .opacity(count > 1 ? 0.8 : 0)
+            
+                Circle()
+                    .frame(width: 11, height: 11)
+                    .foregroundStyle(.precountGradient)
+                    .opacity(count > 2 ? 1 : 0)
+            
+            }
+            
+        }.onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                count += 1
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                count += 1
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                print("end")
+            }
+        }
+    }
+}
+
+#Preview {
+    PrecountView()
+}
