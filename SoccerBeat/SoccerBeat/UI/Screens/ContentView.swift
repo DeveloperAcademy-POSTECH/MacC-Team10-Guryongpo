c032821017f7196d577a0bfb57c2f147d357fcdc
Henrykim1111 <henrykim9319@gmail.com>
2023-11-03 10:31:28 +0900

    feat: [#107] BackButton Custom

--- SoccerBeat/SoccerBeat/UI/Screens/ContentView.swift
+++ SoccerBeat/SoccerBeat/UI/Screens/ContentView.swift
@@ -40,7 +40,9 @@ struct ContentView: View {
                     }
                 }
             }
+            .navigationTitle("")
         }
+        .tint(.white)
         .environmentObject(healthInteractor)
 
     }
