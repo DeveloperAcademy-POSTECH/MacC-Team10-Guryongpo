dfd7c3e82d5958f3880ec805138a02694f6ba15c
Henrykim1111 <henrykim9319@gmail.com>
2023-11-03 12:13:44 +0900

    feat: [#108] 그래프 구현

--- SoccerBeat/SoccerBeat/UI/Screens/BarMinMaxGraphView.swift
+++ SoccerBeat/SoccerBeat/UI/Screens/BarMinMaxGraphView.swift
@@ -9,43 +9,63 @@ import SwiftUI
 import Charts
 
 struct BarMinMaxGraphView: View {
-    var color: Gradient = Gradient(colors: [Color.pink])
-    var data = HeartBeatlast12Months
+    
+    @EnvironmentObject private var healthInteractor: HealthInteractor
+    var graphType: GraphEnum
+    
+    private var mainColor: LinearGradient {
+        switch graphType {
+        case .distance:
+            return .zone2Bpm
+        case .sprint:
+            return .zone3Bpm
+        case .speed:
+            return .zone1Bpm
+        case .heartrate:
+            return .zone4Bpm
+        }
+    }
+
+    private func foo(_ workout: WorkoutData) -> Double {
+        switch graphType {
+        case .distance: return workout.distance
+        case .heartrate: return Double(workout.heartRate["max", default: 0])
+        case .speed: return workout.velocity
+        case .sprint: return Double(workout.sprint)
+        }
+    }
+    
     var body: some View {
             Chart {
-                ForEach(data, id: \.month) {
+                ForEach(healthInteractor.userWorkouts, id: \.id) { workout in
                     BarMark(
-                        x: .value("Month", $0.month, unit: .month),
-                        yStart: .value("Min Sales", $0.dailyMin),
-                        yEnd: .value("Max Sales", $0.dailyMax),
+                        x: .value("Month", workout.dataId ),
+//                        yStart: .value("", graphType == .heartrate ?
+//                                       workout.heartRate["min", default: 0] : 0),
+//                        yEnd: .value("Max Sales", foo(workout)),
+                        yStart: graphType == .heartrate ? CGFloat(workout.heartRate["min", default: 0]) : 0,
+                        yEnd: foo(workout),
                         width: .ratio(0.6)
                     ).cornerRadius(16.5)
                         .opacity(0.6)
-//                    RectangleMark(
-//                        x: .value("Month", $0.month, unit: .month),
-//                        y: .value("Values", $0.dailyAverage),
-//                        width: .ratio(0.6),
-//                        height: .fixed(2)
-//                    )
                 }
-                .foregroundStyle(color)
+                .foregroundStyle(mainColor)
             }
-            .chartYAxis {
-                AxisMarks() { _ in
-                    AxisGridLine().foregroundStyle(.clear)
-                    AxisTick().foregroundStyle(.clear)
-                }
-            }
-            .chartYScale(domain: [60,200])
-            .chartXAxis {
-                AxisMarks() { _ in
-                     AxisGridLine().foregroundStyle(.clear)
-                     AxisTick().foregroundStyle(.clear)
-                }
-            }
+//            .chartYAxis {
+//                AxisMarks() { _ in
+//                    AxisGridLine().foregroundStyle(.clear)
+//                    AxisTick().foregroundStyle(.clear)
+//                }
+//            }
+//            .chartXAxis {
+//                AxisMarks() { _ in
+//                     AxisGridLine().foregroundStyle(.clear)
+//                     AxisTick().foregroundStyle(.clear)
+//                }
+//            }
         }
 }
 
-#Preview {
-    BarMinMaxGraphView()
-}
+//#Preview {
+//    BarMinMaxGraphView(graphType: GraphEnum.distance)
+//}
