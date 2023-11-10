9753e9e3af96b41c7f64c4dee23d6d70a70e976e
geee3 <c1as2icblu3@gmail.com>
2023-11-10 23:30:22 +0900

    del: [#125] NilDataView 삭제 및 화면 구현

--- SoccerBeat/SoccerBeat/UI/Screens/ContentView.swift
+++ SoccerBeat/SoccerBeat/UI/Screens/ContentView.swift
@@ -13,56 +13,68 @@ struct ContentView: View {
     @State var userWorkouts: [WorkoutData]?
     
     var body: some View {
-        if userWorkouts == nil {
-            NilDataView()
-        } else {
-            NavigationStack {
-                ScrollView {
-                    ZStack {
-                        Image("BackgroundPattern")
-                            .resizable()
-                            .scaledToFit()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(height: 0)
+        NavigationStack {
+            ScrollView {
+                ZStack {
+                    Image("BackgroundPattern")
+                        .resizable()
+                        .scaledToFit()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(height: 0)
+                    
+                    VStack {
                         
-                        VStack {
-                            
-                            MyCardView()
-                            
-                            Spacer()
-                                .frame(height: 114) 
-                            
+                        MyCardView()
+                        
+                        Spacer()
+                            .frame(height: 114)
+                        
+                        if userWorkouts == nil {
+                            HStack {
+                                VStack(alignment: .leading, spacing: 0.0) {
+                                    Text("워치를 차고")
+                                    Text("경기를")
+                                    Text("기록해 볼까요?")
+                                }
+                                .foregroundStyle(.linearGradient(colors: [.brightmint, .white], startPoint: .topLeading, endPoint: .bottomTrailing))
+                                .font(.custom("SFProText-HeavyItalic", size: 36))
+                                .kerning(-1.5)
+                                .padding(.leading, 10.0)
+                                Spacer()
+                            }
+                            .padding(.top, 30)
+                            .padding(.horizontal)
+                        } else {
                             MatchRecapView(userWorkouts: $userWorkouts)
-                            
-                            Spacer()
-                                .frame(height: 60)
-                            
-                            AnalyticsView(userWorkouts: $userWorkouts)
-                            
-                            Spacer()
-                                .frame(height: 60)
-                            
                         }
+                        
+                        Spacer()
+                            .frame(height: 60)
+                        
+                        AnalyticsView(userWorkouts: $userWorkouts)
+                        
+                        Spacer()
+                            .frame(height: 60)
+                        
                     }
                 }
-                .navigationTitle("")
             }
-            .task {
-                healthInteractor.requestAuthorization()
-            }
-            .onReceive(healthInteractor.authSuccess, perform: {
-                Task {
-                    print("ContentView: attempting to fetch all data..")
-                    await healthInteractor.fetchAllData()
-                }
-            })
-            .onReceive(healthInteractor.fetchSuccess, perform: {
-                print("ContentView: fetching user data success..")
-                self.userWorkouts = healthInteractor.userWorkouts
-            })
-            .tint(.white)
-            
+            .navigationTitle("")
         }
+        .task {
+            healthInteractor.requestAuthorization()
+        }
+        .onReceive(healthInteractor.authSuccess, perform: {
+            Task {
+                print("ContentView: attempting to fetch all data..")
+                await healthInteractor.fetchAllData()
+            }
+        })
+        .onReceive(healthInteractor.fetchSuccess, perform: {
+            print("ContentView: fetching user data success..")
+            self.userWorkouts = healthInteractor.userWorkouts
+        })
+        .tint(.white)
     }
 }
 
