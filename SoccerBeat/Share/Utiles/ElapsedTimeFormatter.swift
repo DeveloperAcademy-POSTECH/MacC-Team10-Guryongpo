e5af4d3e2189cbc21cdb91e023f3e42626f9668d
Damagucci-Juice <jsr9874@gmail.com>
2023-11-12 15:05:04 +0900

    add: [#135] ElapsedTimeFormatter, TextBorder Swift 파일 추가

---
+++ SoccerBeat/Share/Utiles/ElapsedTimeFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  ElapsedTimeFormatter.swift
+//  SoccerBeat
+//
+//  Created by Gucci on 11/12/23.
+//
+
+import Foundation
---
+++ SoccerBeat/Share/Utiles/TextBorder+extension.swift
@@ -0,0 +1,18 @@
+//
+//  TextBorderModifer.swift
+//  SoccerBeat
+//
+//  Created by Gucci on 11/12/23.
+//
+
+import SwiftUI
+
+struct TextBorderModifer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextBorderModifer()
+}
