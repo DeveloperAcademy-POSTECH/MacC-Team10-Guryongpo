8ecbd650901f47d60a6947fb3c9a0f0701a2569e
Damagucci-Juice <jsr9874@gmail.com>
2023-11-12 15:16:53 +0900

    move: [#135] ElapsedTimeFormatter 별도 파일 분리

--- SoccerBeat/SoccerBeat Watch App/View/Progress/ElapsedTimeView.swift
+++ SoccerBeat/SoccerBeat Watch App/View/Progress/ElapsedTimeView.swift
@@ -19,28 +19,6 @@ struct ElapsedTimeView: View {
             }
         }
     }
-}
-
-class ElapsedTimeFormatter: Formatter {
-    let componentsFormatter: DateComponentsFormatter = {
-        let formatter = DateComponentsFormatter()
-        formatter.allowedUnits = [.minute, .second]
-        formatter.zeroFormattingBehavior = .pad
-        return formatter
-    }()
-
-    override func string(for value: Any?) -> String? {
-        guard let time = value as? TimeInterval else {
-            return nil
-        }
-
-        guard let formattedString = componentsFormatter.string(from: time) else {
-            return nil
-        }
-
-        return formattedString
-    }
-}
 
 private struct Particle: Identifiable {
     var id: UUID = .init()
