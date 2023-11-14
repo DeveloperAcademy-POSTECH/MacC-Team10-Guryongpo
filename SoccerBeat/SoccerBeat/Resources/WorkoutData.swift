4e21ee7ab928dea3eb894d6fa0d1866af44d99c5
Damagucci-Juice <jsr9874@gmail.com>
2023-11-14 17:03:37 +0900

    move: [#148] 현재 위치를 받아오는 코드를 WorkoutData로 이동

--- SoccerBeat/SoccerBeat/Resources/WorkoutData.swift
+++ SoccerBeat/SoccerBeat/Resources/WorkoutData.swift
@@ -14,7 +14,6 @@ struct WorkoutData: Hashable, Equatable, Identifiable {
     let date: String
     let time: String
     let distance: Double // total distance
-    let location: String
     let sprint: Int // sprint counter
     let velocity: Double // maximum velocity. km/h
     var heartRate: [String: Int] // min, max of heartRate. ex) ["max": 00, "min": 00]
@@ -29,5 +28,43 @@ extension CLLocationCoordinate2D: Hashable {
     
     public func hash(into hasher: inout Hasher) {
         hasher.combine(self.latitude) 
+    }
+}
+
+
+// MARK: - 현재 위치 받아오는 코드
+extension WorkoutData {
+    var location: String {
+        get async throws {
+            guard let last = route.last else { return "마지막 위치가 지정되지 않았습니다." }
+            guard let loadedAddress = try? await showCurrentAddress(last) else { return "주소값을 변환하는데 실패했습니다." }
+            return loadedAddress
+        }
+    }
+    
+    private func showCurrentAddress(_ location: CLLocationCoordinate2D?) async throws -> String {
+        guard let position = location else { return "" }
+        let locale = Locale(identifier: "Ko-kr")
+        let geoCoder = CLGeocoder()
+        
+        let location : CLLocation = CLLocation(latitude: position.latitude, longitude: position.longitude)
+        
+        var currentAddress = ""
+        guard let marker = try await geoCoder.reverseGeocodeLocation(location, preferredLocale: locale).first
+            else { return "" }
+        
+        if let administrativeArea = marker.administrativeArea {
+            currentAddress += administrativeArea + " "
+        }
+        if let locality = marker.locality {
+            currentAddress += locality + " "
+        }
+        if let subLocality = marker.subLocality {
+            currentAddress += subLocality + " "
+        }
+        if let subThoroughfare = marker.subThoroughfare {
+            currentAddress += subThoroughfare + " "
+        }
+        return currentAddress
     }
 }
