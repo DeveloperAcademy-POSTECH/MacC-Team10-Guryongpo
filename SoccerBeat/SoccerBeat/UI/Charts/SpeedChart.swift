4a8364f582fd4f1b72bb284b6780df75a4674494
Damagucci-Juice <jsr9874@gmail.com>
2023-11-15 23:19:18 +0900

    feat: [#136] 실제 사용하는 데이터 타입 붙이고 피그마에 맞춰서 뷰 짜기

--- SoccerBeat/SoccerBeat/UI/Charts/SpeedChart.swift
+++ SoccerBeat/SoccerBeat/UI/Charts/SpeedChart.swift
@@ -9,36 +9,53 @@ import SwiftUI
 import Charts
 
 struct SpeedChart: View {
-    var data: [Series] = SpeedDummyData
+    let workouts: [WorkoutData]
+    let fastestWorkout: WorkoutData
+    let slowestWorkout: WorkoutData
+    let averageSpeed: Double
+    
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "Ko-kr")
+        formatter.dateFormat = "yyyy.MM.dd"
+        return formatter
+    }()
+    
+    private func isMax(_ workout: WorkoutData) -> Bool {
+        workout == fastestWorkout
+    }
+    
+    private func isMin(_ workout: WorkoutData) -> Bool {
+        workout == slowestWorkout
+    }
+    
     var body: some View {
         Chart {
-            ForEach(data) { series in
-                ForEach(0..<series.values.count, id: \.self) { index in
-                    let element = series.values[index]
-                    BarMark(
-                        x: .value("Day", element.day, unit: .day),
-                        yStart: .value("Values", 1),
-                        yEnd: .value("Values", element.values),
-                        width: .ratio(0.5)
-                    )
-                    .foregroundStyle(.zone1Bpm) // TODO: - max 면 틴트, 아니면 흰색
-                    .cornerRadius(300, style: .continuous)
-                    // MARK: - 데이터 표시
-                    .annotation(position: .bottom, alignment: .center) {
-                        if index.isMultiple(of: 2) {
-                            Text("\(String(format: "%.1f", element.values))km/h")
-                                .font(.caption2)
-                        }
+            ForEach(0..<workouts.count, id: \.self) { index in
+                let workout = workouts[index]
+                let thisDate = dateFormatter.date(from: workout.date) ?? Date()
+                BarMark(
+                    x: .value("Day", thisDate, unit: .day),
+                    yStart: .value("Velocity", 1),
+                    yEnd: .value("Velocity", workout.velocity),
+                    width: .ratio(0.5)
+                )
+                .foregroundStyle(isMax(workout) ? .speedMax : (isMin(workout) ? .speedMin : .chartDefault))
+                .cornerRadius(300, style: .continuous)
+                // MARK: - Min, Max 표시
+                .annotation(position: .top, alignment: .center) {
+                    if isMax(workout) {
+                        Text("\(String(format: "%.1f", workout.velocity))")
+                            .font(.maxHighlight)
                     }
                 }
             }
         }
         // MARK: - 가장 밑에 일자 표시
         .chartXAxis {
-            AxisMarks(values: .stride(by: .day)) { xValue in
-                if xValue.index.isMultiple(of: 2) {
-                    AxisValueLabel(format: .dateTime.day(), centered: true)
-                }
+            AxisMarks(values: .stride(by: .day)) { _ in
+                AxisValueLabel(format: .dateTime.day(), centered: true)
+                    .font(.dayUnit)
             }
         }
         .chartYAxis(.hidden)
@@ -46,50 +63,144 @@ struct SpeedChart: View {
 }
 
 struct SpeedChartView: View {
+    let workouts: [WorkoutData]
+    private var startDate: String {
+        workouts.first?.date ?? "2023.10.10"
+    }
+    private var endDate: String {
+        workouts.last?.date ?? "2023.10.10"
+    }
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("최고 속도")
-                .padding(.leading, 14)
-            ZStack {
-                LightRectangleView()
-                    .frame(height: 200)
-                    .overlay {
-                        VStack {
-                            Text("2023.10.02-2023.10.10")
-                                .padding(.top, 14)
-                            Spacer()
-                            SpeedChart()
-                                .frame(height: 120)
-                                .padding(.horizontal, 16)
-                        }
-                    }
-            }
-            
-            LightRectangleView()
-                .frame(height: 100)
-                .overlay {
-                    HStack {
-                        Spacer()
-                        VStack(alignment: .leading) {
-                            Text("최고 기록")
-                            Text("2023.10.12")
-                            Text("21Km/h")
-                        }
-                        Spacer()
-                        VStack(alignment: .leading) {
-                            Text("최저 기록")
-                            Text("2023.10.17")
-                            Text("15Km/h")
-                        }
-                        Spacer()
-                    }
+        let fastest = maximum(of: workouts)
+        let slowest = minimum(of: workouts)
+        
+        return ZStack {
+            BackgroundImageView()
+
+            VStack(alignment: .leading) {
+                Spacer(minLength: 50)
+                
+                Text("# 나의 최고 속도를 찾아볼까요?")
+                    .floatingCapsuleStyle()
+                    .padding(.leading, -10)
+                
+                Group {
+                    Text("My speed")
+                        .font(.navigationSportySubTitle)
+                        .foregroundStyle(.navigationSportyHead)
+                    Text("The quality of  a")
+                    Text("FW (Km/h)")
+                        .foregroundStyle(.navigationSportySpeedTitle)
                 }
-            
-            
+                .font(.navigationSportyTitle)
+                
+                speedChartView(fastest: fastest, slowest: slowest)
+                
+                averageSpeedView
+                
+                Spacer(minLength: 300)
+            }
         }
+        .padding(.horizontal, 28)
     }
 }
 
 #Preview {
-    SpeedChartView()
+    NavigationStack {
+        SpeedChartView(workouts: fakeWorkoutData)
+    }
+}
+// MARK: - Data Analyze Protocol
+extension SpeedChartView: Analyzable {
+    func maximum(of workouts: [WorkoutData]) -> WorkoutData {
+        guard var maximumSpeedWorkout = workouts.first else { return WorkoutData.example }
+        for workout in workouts where maximumSpeedWorkout.velocity < workout.velocity {
+            maximumSpeedWorkout = workout
+        }
+        return maximumSpeedWorkout
+    }
+    
+    func minimum(of workouts: [WorkoutData]) -> WorkoutData {
+        guard var minimumSpeedWorkout = workouts.first else { return WorkoutData.example }
+        for workout in workouts where minimumSpeedWorkout.velocity > workout.velocity {
+            minimumSpeedWorkout = workout
+        }
+        return minimumSpeedWorkout
+    }
+    
+    func average(of workouts: [WorkoutData]) -> Double {
+        var velocitySum = 0.0
+        workouts.forEach { workout in
+            velocitySum += workout.velocity
+        }
+        return velocitySum / Double(workouts.count)
+    }
+}
+
+// MARK: - UI
+extension SpeedChartView {
+    
+    private func speedChartView(fastest: WorkoutData, slowest: WorkoutData) -> some View {
+        LightRectangleView()
+            .frame(height: 200)
+            .overlay {
+                VStack {
+                    Group {
+                        Text("\(startDate)-\(endDate)")
+                            .padding(.top, 14)
+                        Text("(KM)")
+                    }
+                    .font(.durationStyle)
+                    .foregroundStyle(.durationUnit)
+                    
+                    Spacer()
+                    SpeedChart(
+                        workouts: workouts,
+                        fastestWorkout: fastest,
+                        slowestWorkout: slowest,
+                        averageSpeed: average(of: workouts)
+                    )
+                    .frame(height: 120)
+                    .padding(.horizontal, 16)
+                }
+            }
+    }
+    
+    @ViewBuilder
+    private var averageSpeedView: some View {
+        LightRectangleView()
+            .frame(height: 100)
+            .padding(.horizontal, 16)
+            .overlay {
+                VStack {
+                    Text("해리케인의 평균 속도는 21 Km/h입니다.")
+                        .opacity(0.7)
+                    Text("\(workouts.count) 경기 평균")
+                    Group {
+                        Text(average(of: workouts), format: .number)
+                        + Text("Km/h")
+                    }
+                    .font(.averageValue)
+                    .foregroundStyle(.navigationSportySpeedTitle)
+                }
+            }
+    }
+}
+
+struct FloatingCapsuleModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(.horizontal, 8)
+            .padding(.vertical, 4)
+            .overlay {
+                Capsule()
+                    .strokeBorder(Color(hex: 0x757575, alpha: 0.4), lineWidth: 0.8)
+            }
+    }
+}
+
+extension View {
+    func floatingCapsuleStyle() -> some View {
+        modifier(FloatingCapsuleModifier())
+    }
 }
