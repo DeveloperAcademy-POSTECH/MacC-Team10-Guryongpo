47c88e129e3cc9736478cdf534bc70210f13597a
Damagucci-Juice <jsr9874@gmail.com>
2023-11-16 02:23:55 +0900

    fix: [#136] 목업 데이터의 스프린트 부분 정상화

--- SoccerBeat/SoccerBeat/Resources/WorkoutData.swift
+++ SoccerBeat/SoccerBeat/Resources/WorkoutData.swift
@@ -87,12 +87,12 @@ extension WorkoutData {
 
 let fakeWorkoutData: [WorkoutData] = [
     WorkoutData(dataID: 1, date: "2023.10.09", time: "60:10", distance: 3.5, sprint: 3, velocity: 10.5, heartRate: ["max": 171, "min": 53], route: [], center: [0, 0]),
-    WorkoutData(dataID: 2, date: "2023.10.10", time: "60:10", distance: 2.1, sprint: 3, velocity: 11.5, heartRate: ["max": 152, "min": 70], route: [], center: [0, 0]),
-    WorkoutData(dataID: 3, date: "2023.10.11", time: "60:10", distance: 1.1, sprint: 3, velocity: 8.5, heartRate: ["max": 167, "min": 92], route: [], center: [0, 0]),
-    WorkoutData(dataID: 4, date: "2023.10.12", time: "60:10", distance: 5.1, sprint: 3, velocity: 12.5, heartRate: ["max": 185, "min": 100], route: [], center: [0, 0]),
-    WorkoutData(dataID: 5, date: "2023.10.13", time: "60:10", distance: 4.5, sprint: 3, velocity: 17.2, heartRate: ["max": 175, "min": 60], route: [], center: [0, 0]),
-    WorkoutData(dataID: 6, date: "2023.10.14", time: "60:10", distance: 3.6, sprint: 3, velocity: 24.4, heartRate: ["max": 190, "min": 79], route: [], center: [0, 0]),
-    WorkoutData(dataID: 7, date: "2023.10.15", time: "60:10", distance: 3.8, sprint: 3, velocity: 15.9, heartRate: ["max": 183, "min": 91], route: [], center: [0, 0]),
-    WorkoutData(dataID: 8, date: "2023.10.16", time: "60:10", distance: 2.9, sprint: 3, velocity: 17.3, heartRate: ["max": 169, "min": 79], route: [], center: [0, 0]),
-    WorkoutData(dataID: 9, date: "2023.10.17", time: "60:10", distance: 5.3, sprint: 3, velocity: 23.5, heartRate: ["max": 187, "min": 60], route: [], center: [0, 0])
+    WorkoutData(dataID: 2, date: "2023.10.10", time: "60:10", distance: 2.1, sprint: 5, velocity: 11.5, heartRate: ["max": 152, "min": 70], route: [], center: [0, 0]),
+    WorkoutData(dataID: 3, date: "2023.10.11", time: "60:10", distance: 1.1, sprint: 7, velocity: 8.5, heartRate: ["max": 167, "min": 92], route: [], center: [0, 0]),
+    WorkoutData(dataID: 4, date: "2023.10.12", time: "60:10", distance: 5.1, sprint: 9, velocity: 12.5, heartRate: ["max": 185, "min": 100], route: [], center: [0, 0]),
+    WorkoutData(dataID: 5, date: "2023.10.13", time: "60:10", distance: 4.5, sprint: 11, velocity: 17.2, heartRate: ["max": 175, "min": 60], route: [], center: [0, 0]),
+    WorkoutData(dataID: 6, date: "2023.10.14", time: "60:10", distance: 3.6, sprint: 5, velocity: 24.4, heartRate: ["max": 190, "min": 79], route: [], center: [0, 0]),
+    WorkoutData(dataID: 7, date: "2023.10.15", time: "60:10", distance: 3.8, sprint: 13, velocity: 15.9, heartRate: ["max": 183, "min": 91], route: [], center: [0, 0]),
+    WorkoutData(dataID: 8, date: "2023.10.16", time: "60:10", distance: 2.9, sprint: 17, velocity: 17.3, heartRate: ["max": 169, "min": 79], route: [], center: [0, 0]),
+    WorkoutData(dataID: 9, date: "2023.10.17", time: "60:10", distance: 5.3, sprint: 12, velocity: 23.5, heartRate: ["max": 187, "min": 60], route: [], center: [0, 0])
 ]
