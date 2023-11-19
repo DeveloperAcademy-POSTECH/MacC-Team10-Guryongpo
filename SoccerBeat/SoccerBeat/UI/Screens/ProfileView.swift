1dd0c5fbc7e7cb796084732ef2668a6a2fc1844b
geee3 <c1as2icblu3@gmail.com>
2023-11-20 05:06:32 +0900

    feat: [#175] ProfileView layout 설정

--- SoccerBeat/SoccerBeat/UI/Screens/ProfileView.swift
+++ SoccerBeat/SoccerBeat/UI/Screens/ProfileView.swift
@@ -14,35 +14,74 @@ struct ProfileView: View {
     
     var body: some View {
         ScrollView {
-            VStack {
-                HStack {
-                    VStack(alignment: .leading, spacing: 0.0) {
-                        HStack(spacing: 0.0) {
-                            Text("Hello, ")
-                            TextField("Name", text: $userName)
-                                .onChange(of: userName) { _ in
-                                    UserDefaults.standard.set(userName, forKey: "userName")
-                                }
-                        }
-                        Text("How you like that?")
+            ZStack {
+                BackgroundImageView()
+                
+                VStack {
+                    HStack {
+                        Text("# 이번 경기 지표를 내 평균 능력치와 비교해 볼까요?")
+                            .floatingCapsuleStyle()
+                            .padding(.horizontal)
+                        
+                        Spacer()
                     }
-                    .foregroundStyle(!isFlipped ?
-                        .linearGradient(colors: [.brightmint, .white], startPoint: .topLeading, endPoint: .bottomTrailing) : .linearGradient(colors: [.titlegray, .white], startPoint: .topLeading, endPoint: .bottomTrailing))
-                    .font(.custom("SFProText-HeavyItalic", size: 36))
-                    .kerning(-1.5)
-                    .padding(.leading, 10.0)
+                    
+                    HStack {
+                        VStack(alignment: .leading, spacing: 0.0) {
+                            HStack(spacing: 0.0) {
+                                Text("Hello, ")
+                                TextField("Name", text: $userName)
+                                    .onChange(of: userName) { _ in
+                                        UserDefaults.standard.set(userName, forKey: "userName")
+                                    }
+                            }
+                            Text("How you like that?")
+                        }
+                        .font(.custom("SFProText-HeavyItalic", size: 36))
+                        .kerning(-1.5)
+                        .padding(.leading, 10.0)
+                        Spacer()
+                    }
+                    .padding(.top, 30)
+                    .padding(.horizontal)
+                    
                     Spacer()
+                        .frame(height: 80)
+                    
+                    MyCardView(isFlipped: $isFlipped, viewModel: viewModel)
+                    PhotoSelectButtonView(viewModel: viewModel)
+                        .opacity(isFlipped ? 1 : 0)
+                        .padding()
+                    
+                    let average = [3.0, 2.4, 3.4, 3.2, 2.8, 3.3]
+                    let recent = [4.1, 3.0, 3.5, 3.8, 3.5, 2.8]
+                    ViewControllerContainer(RadarViewController(radarAverageValue: average, radarAtypicalValue: recent))
+                        .fixedSize()
+                        .frame(width: 210, height: 210)
+                    
+                    Spacer().frame(height: 100)
+                    
+                    HStack {
+                        Text("# 이번 경기 지표를 내 평균 능력치와 비교해 볼까요?")
+                            .floatingCapsuleStyle()
+                            .padding(.horizontal)
+                        
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        VStack(alignment: .leading, spacing: 0.0) {
+                            Text("My")
+                            Text("Card Collection")
+                        }
+                        .font(.custom("SFProText-HeavyItalic", size: 36))
+                        .kerning(-1.5)
+                        .padding(.leading, 10.0)
+                        Spacer()
+                    }
+                    .padding(.top, 30)
+                    .padding(.horizontal)
                 }
-                .padding(.top, 30)
-                .padding(.horizontal)
-                
-                Spacer()
-                    .frame(height: 80)
-                
-                MyCardView(isFlipped: $isFlipped, viewModel: viewModel)
-                PhotoSelectButtonView(viewModel: viewModel)
-                    .opacity(isFlipped ? 1 : 0)
-                    .padding()
             }
         }.onAppear {
             userName = UserDefaults.standard.string(forKey: "userName") ?? ""
