66632362c5c77afde311622eb314f11c508dd7cc
Damagucci-Juice <jsr9874@gmail.com>
2023-11-20 11:40:48 +0900

    move: [#172] ActivityEnum 위치 조정

---
+++ SoccerBeat/Share/Model/ActivityEnum.swift
@@ -0,0 +1,15 @@
+//
+//  ActivityEnum.swift
+//  SoccerBeat
+//
+//  Created by Gucci on 11/20/23.
+//
+
+import Foundation
+
+enum ActivityEnum: CaseIterable {
+    case distance
+    case sprint
+    case speed
+    case heartrate
+}
