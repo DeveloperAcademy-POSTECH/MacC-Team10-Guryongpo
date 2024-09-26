6a30e6b0d74527e01b47825c458f7b421a307067
Hyungmin Kim <106143629+hyungminkimdev@users.noreply.github.com>
2024-09-26 09:29:32 -0400

    feat: game time text fixed (#436)
    * feat: game time text fixed
    
    * feat: preview en deleted

--- SoccerBeat/SoccerBeat/Sources/Features/HomeView/MainView.swift
+++ SoccerBeat/SoccerBeat/Sources/Features/HomeView/MainView.swift
@@ -183,6 +183,8 @@ struct MainView: View {
                                         }
                                     Group {
                                         Text("경기 시간")
+                                            .lineLimit(1)
+                                            .minimumScaleFactor(0.5)
                                         Group {
                                             if !workouts.isEmpty {
                                                 Text(workouts[0].time)
