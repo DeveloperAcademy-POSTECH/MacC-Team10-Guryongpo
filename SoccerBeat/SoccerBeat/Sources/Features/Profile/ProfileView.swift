41a8c59ffeb90036bd50ef8a63089f768e0bff17
Henrykim1111 <henrykim9319@gmail.com>
2024-10-04 10:47:28 -0400

    padding 수정

--- SoccerBeat/SoccerBeat/Sources/Features/Profile/ProfileView.swift
+++ SoccerBeat/SoccerBeat/Sources/Features/Profile/ProfileView.swift
@@ -119,6 +119,7 @@ struct ProfileView: View {
                     TrophyCollectionView()
                     
                 }
+                .padding(.horizontal)
                 .onTapGesture {
                     hideKeyboard()
                 }
