bfb1c6771bfacee920cbc0165fb341b708b2e8b1
Damagucci-Juice <50472122+Damagucci-Juice@users.noreply.github.com>
2024-10-31 23:17:45 +0900

    feat: Stationary 기능 구현 (#450)
    * feat: Stationary 기능 구현
    
    * fix: Stationary 기능의 로컬라이제이션 구현

---
+++ SoccerBeat/SoccerBeat Watch App/Sources/Features/StartPauseControl/DetactStationaryView.swift
@@ -0,0 +1,80 @@
+//
+//  DetactStationaryView.swift
+//  SoccerBeat Watch App
+//
+//  Created by Gucci on 10/10/24.
+//
+
+import SwiftUI
+
+struct DetactStationaryView: View {
+    @Environment(\.dismiss) var dismiss
+    @EnvironmentObject var workoutManager: WorkoutManager
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("Finished\nyour workout?")
+                    .font(.headline)
+                    .overlay {
+                        Image(.soccerbeatHeart)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40, height: 40)
+                            .offset(x: 100)
+                    }
+
+                Spacer()
+
+                // "End Workout" button
+                Button {
+                    workoutManager.endWorkout()
+                    dismiss()
+                } label: {
+                    Text("경기 종료")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(.mint)
+                        .cornerRadius(12)
+                }
+                .padding(.bottom, 10)
+
+                // "Pause" button
+                Button {
+                    workoutManager.togglePause()
+                    dismiss()
+                } label: {
+                    Text("일시 정지")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(.regularMaterial)
+                        .cornerRadius(12)
+                }
+                .padding(.bottom, 10)
+
+                // "Close" button
+                Button {
+                    dismiss()
+                } label: {
+                    Text("close")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(.regularMaterial)
+                        .cornerRadius(12)
+                }
+            }
+            .padding(.horizontal)
+        }
+        .scrollIndicators(.never)
+    }
+}
+
+//#Preview {
+//    DetactStationaryView()
+//}
