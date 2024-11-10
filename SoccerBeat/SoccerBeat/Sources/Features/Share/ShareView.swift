fa8f626e7db61bc5fe3fe4aa14ca5edc5437b166
Henrykim1111 <henrykim9319@gmail.com>
2024-11-10 01:10:32 -0500

    fix: 쉐어뷰 백버튼 수정

--- SoccerBeat/SoccerBeat/Sources/Features/Share/ShareView.swift
+++ SoccerBeat/SoccerBeat/Sources/Features/Share/ShareView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 import Photos
 
 struct ShareView: View {
+    @Environment(\.dismiss) private var dismiss
     @Binding var isBackButtonHidden: Bool
     @EnvironmentObject var profileModel: ProfileModel
     @EnvironmentObject var workoutManager: WorkoutManager
@@ -57,20 +58,32 @@ struct ShareView: View {
             }
             .padding()
         }
-        .navigationBarBackButtonHidden(isBackButtonHidden)
+        .navigationBarBackButtonHidden()
         .toolbar {
-            Button {
-                isBackButtonHidden = true
-                DispatchQueue.main.async {
-                            share()
-                        }
-                DispatchQueue.main.async {
-                    isBackButtonHidden = false
-                        }
-            } label: {
-                Text("공유하기")
-                    .foregroundStyle(.shareViewTitleTint)
-                    .opacity(isBackButtonHidden ? 0 : 1)
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "chevron.backward")
+                        .foregroundStyle(Color.white)
+                        .opacity(isBackButtonHidden ? 0 : 1)
+                }
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    isBackButtonHidden = true
+                    DispatchQueue.main.async {
+                                share()
+                            }
+                    DispatchQueue.main.async {
+                        isBackButtonHidden = false
+                            }
+                } label: {
+                    Text("공유하기")
+                        .foregroundStyle(.shareViewTitleTint)
+                        .opacity(isBackButtonHidden ? 0 : 1)
+                }
             }
         }
     }
