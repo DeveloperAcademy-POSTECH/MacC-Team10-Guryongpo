e996ee81a6ec4a1c1f8ab7e1d2d3ad334bcc6463
Damagucci-Juice <jsr9874@gmail.com>
2025-01-06 19:59:19 +0900

    feat: 뷰에서 버튼 UI에 맞춰서 선언

--- SoccerBeat/SoccerBeat/Sources/Features/Share/ShareMatchView.swift
+++ SoccerBeat/SoccerBeat/Sources/Features/Share/ShareMatchView.swift
@@ -14,6 +14,7 @@ struct ShareMatchView: View {
 
     var body: some View {
         VStack {
+            // dismiss button
             HStack {
                 Spacer()
 
@@ -28,7 +29,73 @@ struct ShareMatchView: View {
                 .padding([.top, .trailing], 10)
             }
 
+            HStack {
+                InformationButton(message: "지도를 움직여 공유할 위치를 선택하세요!")
+                Spacer()
+            }
+            .padding(.leading, 39)
+
+            // photo card, shareing image
+            Color.white
+                .frame(height: 560)
+                .padding(.top, 16)
+                .padding(.horizontal, 39)
+
             Spacer()
+
+            // 스토리 공유
+            // share image
+            Button {
+                // share story action
+                
+            } label: {
+                HStack {
+                    Image(systemName: "x.circle")
+
+                    Spacer()
+                        .frame(width: 32)
+
+                    Text("스토리 공유하기")
+                        .font(.shareButtonFont)
+
+                }
+                .frame(maxWidth: .infinity)
+                .padding(10)
+                .foregroundStyle(.white)
+                .background(
+                    Capsule()
+                        .stroke(.grayGradient, lineWidth: 1)  // 그라디언트 포인트 디테일 살릴 필요 있음
+                        .fill(.shareButtonTint)
+                )
+                .padding(.horizontal, 18)
+            }
+
+            // 이미지 저장
+            // store image
+            Button {
+                // store action
+
+            } label: {
+                HStack {
+                    Image(systemName: "square.and.arrow.down.fill")
+                        .foregroundStyle(.navigationSportyBPMTitle)
+
+                    Spacer()
+                        .frame(width: 32)
+
+                    Text("이미지 저장하기")
+                        .font(.shareButtonFont)
+                }
+                .frame(maxWidth: .infinity)
+                .padding(10)
+                .foregroundStyle(.white)
+                .background(
+                    Capsule()
+                        .stroke(.grayGradient, lineWidth: 1)
+                        .fill(.shareButtonTint)
+                )
+                .padding(.horizontal, 18)
+            }
         }
     }
 }
