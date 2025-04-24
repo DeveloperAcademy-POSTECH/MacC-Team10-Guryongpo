fa0c6af388a3002ebca9a4c00e04ff7c5243f30a
Damagucci-Juice <jsr9874@gmail.com>
2025-04-24 22:41:16 +0900

    Revert "temp: 위치조정"
    This reverts commit 6af19b706ab43116fa22910b2dc7100fd365a2a4.

--- SoccerBeat/SoccerBeat/Sources/Features/Share/ShareMatchView.swift
+++ SoccerBeat/SoccerBeat/Sources/Features/Share/ShareMatchView.swift
@@ -87,7 +87,6 @@ struct ShareMatchView: View {
             // store image
             Button {
                 // MARK: - MapView 저장
-                let imageSaver = ImageSaver()
                 /**
                  shootWithMap { image in
                  imageSaver.writeToPhotoAlbum(image: image)
@@ -95,6 +94,7 @@ struct ShareMatchView: View {
                  }
                  */
 
+                let imageSaver = ImageSaver()
                 // MARK: - Screen Shot 저장
                 let inputImage = snapshot()
                 imageSaver.writeToPhotoAlbum(image: inputImage)
